72ff0c41a1b6e2eb150a384e0626d14d9e7c9840
thomkaar <thomkaar@gmail.com>
2019-02-06 12:02:56 -0800

    Added script to create lc_unit table and ported to python3

--- laCroixCalc.py
+++ laCroixCalc.py
@@ -16,7 +16,7 @@ BERRY_PRCNT = 0.25
 CRAN_RASPBERRY_PRCNT = 0.25 
 SINGLE_FLAVOR_PRCNT = 1.0
    
-
+FLAVOR_ARRAY = ["Pure", "Lime", "Lemon", "Orange", "Berry", "Cran-Raspberry", "Pamplemousse", "Peach-Pear", "Coconut", "Apricot", "Passionfruit", "Mango", "Tangerine", "Key Lime"]
 
 def main():
    #Smart and Final variables
@@ -43,9 +43,9 @@ def main():
    vonsRatio = calcPriceRatio(vonsPrice, vonsQuantity, vonsPackSize)
    costcoRatio = calcPriceRatio(costcoPrice, costcoQuantity, costcoPackSize)
     
-   print "Smart Ratio is: " + str(smartRatio)
-   print "Vons Ratio is: " + str(vonsRatio)
-   print "Costco Ratio is: " + str(costcoRatio)
+   print("Smart Ratio is: " + str(smartRatio))
+   print("Vons Ratio is: " + str(vonsRatio))
+   print("Costco Ratio is: " + str(costcoRatio))
    """
  
    introInfo()
@@ -53,12 +53,13 @@ def main():
    while(continue_flag == 'y'):
    
       flavorProfile = collectFlavorProfile()
- 
+      favoriteFlavor = findFavoriteFlavor(flavorProfile)
+      print("\nYour favorite LaCroix flavor is: " + favoriteFlavor)
       """
       The following is the calculations needed for a finding the ValueRatio for packs of La Croix containing only one flavor
       The ValueRatio = price of package / (tasteCoefficient of flavor X * # of flavor X in the package)
-      """
-      tasteCoefficient = max(flavorProfile)
+      """      
+      tasteCoefficient = max(flavorProfile) 
       flavorPackageRatio = SINGLE_FLAVOR_PRCNT
       packagePleasantness = calcPleasantness(tasteCoefficient,flavorPackageRatio)
       smartValueRatio = calcValueRatio(smartPrice, packagePleasantness, smartQuantity * smartPackSize)
@@ -70,26 +71,26 @@ def main():
       The ValueRatio = price of package / summation(tasteCoefficient of flavor X * # of flavor X in the package)
       """
    
-      availableIndexes = buildAvailableIndexesMixed(flavorProfile)
       #for index in availableIndexes:
-      #   print '(' + str(index[0]) + ',' +  str(index[1]) + ')'
+      #   print '(' + str(index[0]) + ',' +  str(index[1]) + ')'     
+      availableIndexes, tasteCoefficientMixed = buildAvailableIndexesMixed(flavorProfile)
       packagePleasantnessMixed = calcMixedpackagePleasantness(flavorProfile,availableIndexes, costcoPackSize)
       #print "packagePleasantnessMixed: " + str(packagePleasantnessMixed)
       costcoValueRatio = calcValueRatioMixed(costcoPrice, packagePleasantnessMixed)   
   
-      print "\n\nThe following ratios are price per can of La Croix scaled to your taste preferences:" 
-      print "   smartValueRatio:  " + str(smartValueRatio)
-      print "   vonsValueRatio:   " + str(vonsValueRatio)
-      print "   costcoValueRatio: " + str(costcoValueRatio)  
-      print " "
+      print("\n\nThe following ratios are price per can of La Croix scaled to your taste preferences:")
+      print("   smartValueRatio:  " + str(smartValueRatio))
+      print("   vonsValueRatio:   " + str(vonsValueRatio))
+      print("   costcoValueRatio: " + str(costcoValueRatio))  
       if smartValueRatio >  vonsValueRatio and vonsValueRatio < costcoValueRatio:
-         print "Smart and Final La Croix and Costco La Croix are inferior.\nThe best bang for your buck is Vons La Croix."
+         print("Smart and Final La Croix and Costco La Croix are inferior.\nThe best bang for your buck is Vons " + favoriteFlavor + " La Croix.")
       elif vonsValueRatio > smartValueRatio and smartValueRatio < costcoValueRatio:
-         print "Vons La Croix and Costco La Croix are the weaker choices.\nThe bubblier choice is Smart and Final La Croix"
+         print("Vons La Croix and Costco La Croix are the weaker choices.\nThe bubblier choice is Smart and Final " + favoriteFlavor + " La Croix")
       elif costcoValueRatio < smartValueRatio and costcoValueRatio < vonsValueRatio:
-         print "Costco La Croix is the cat's meow.\nAll other La Croix choices can't compare to it's purrfection." 
+         print("Costco La Croix is the cat's meow.\nAll other La Croix choices can't compare to it's purrfection.")
+         print("It would be a catastrophie if you didn't buy the " + FLAVOR_ARRAY[availableIndexes[0]] +  ", " + FLAVOR_ARRAY[availableIndexes[1]] + ", and " + FLAVOR_ARRAY[availableIndexes[2]] + " pack.")
 
-      continue_flag = raw_input("Would you like to run the script again (y/n)")
+      continue_flag = input("Would you like to run the script again? (y/n): ")
    
 #determines the value of a LaCroix factoring in taste preferences
 def calcValueRatio(price, packagePleasantness, quantity):
@@ -118,16 +119,15 @@ def calcMixedpackagePleasantness(flavorProfile, availableIndexes, quantity):
 def collectFlavorPreference(flavor):
    input_string = "notadigit"
    while not input_string.isdigit():
-      input_string = raw_input('\nPlease rate your eagerness to drink ' + flavor + ' La Croix on a scale of 1 to 10: ')
+      input_string = input('\nPlease rate your eagerness to drink ' + flavor + ' La Croix on a scale of 1 to 10: ')
       if(input_string.isdigit() == False or int(input_string) > 10 or int(input_string) < 1):
-         print "Input not recognized as a number please put in an integer."   
+         print("Input not recognized as a number please put in an integer.")
    return int(input_string)/10.0
 
 #creates an array of scalars for each noteworthy LaCroix flavor to represent taste preferences
 def collectFlavorProfile():
-   flavor_array = ["Pure", "Lime", "Lemon", "Orange", "Berry", "Cran-Rasperry", "Pamplemousse", "Peach-Pear", "Coconut", "Mango", "Apricot", "Passionfruit", "Mango", "Tangerine", "Key Lime"]
    flavor_profile = array('f')
-   for flavor in flavor_array:
+   for flavor in FLAVOR_ARRAY:
       flavor_profile.append(collectFlavorPreference(flavor))
    return flavor_profile
 
@@ -145,6 +145,9 @@ def buildAvailableIndexesMixed(flavorProfile):
 
 
 #determines which mixed package the user will like more
+#returns a tuple containing:
+#   0 - the array of tuples with each index into the flavorProfile and percent of the package the flavor takes up
+#   1 - the calculated average tasteCoefficient of the package
 def findMaxofMixed(mixed0, mixed1, flavorProfile):
    mixed0val = 0
    mixed1val = 0
@@ -153,19 +156,31 @@ def findMaxofMixed(mixed0, mixed1, flavorProfile):
    for flavor in mixed1:
       mixed1val += flavorProfile[flavor[0]] * flavor[1]
    if mixed0val > mixed1val:
-      return mixed0
+      return mixed0, mixed0val
    else:
-       return mixed1
+       return mixed1, mixed1val
+
+#determines the index of the user's favorite flavor of LaCroix
+def findFavoriteFlavor(flavorProfile):
+   i = 0
+   max = 0
+   max_index = 0
+   for flavor in flavorProfile:
+      if(flavor > max):
+         max = flavor
+         max_index = i
+      i += 1 
+   return FLAVOR_ARRAY[max_index] 
 
 #prints introduction info
 def introInfo():
-   print "\n\nThis script's purpose is to help users determine where they should make their next "
-   print "LaCroix purchase between the locations of Smart and Final, Vons, and Costco in San "
-   print "Luis Obispo CA. The script will first ask you 15 questions to access your taste "
-   print "preferences for LaCroix and then it will indicate to you which grocer you should "
-   print "make your next purchase at. Enjoy and happy sipping."
+   print("\n\nThis script's purpose is to help users determine where they should make their next ")
+   print("LaCroix purchase between the locations of Smart and Final, Vons, and Costco in San ")
+   print("Luis Obispo CA. The script will first ask you 15 questions to access your taste ")
+   print("preferences for LaCroix and then it will indicate to you which grocer you should ")
+   print("make your next purchase at. Enjoy and happy sipping.")
 
-   print "\n------------------------------------------------------------------------------------\n"   
+   print("\n------------------------------------------------------------------------------------\n")
 
 if __name__ == "__main__":
    main()
